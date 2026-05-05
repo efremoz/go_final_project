97f13f7c904180dea6f6457a80d4c8fff1899046
Efremov Denis <efremoz@gmail.com>
2026-05-05 15:54:54 +0300

    Шаг 2. Проектируем и создаём БД

--- main.go
+++ main.go
@@ -4,17 +4,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"go_final_project/pkg/db"
 )
 
 func main() {
 	const port = 7540
 	const webDir = "./web"
 
+	dbFile := "scheduler.db"
+
+	err := db.Init(dbFile)
+	if err != nil {
+		db.Close()
+		log.Fatal("Ошибка инициализации БД: ", err)
+	}
+
 	http.Handle("/", http.FileServer(http.Dir(webDir)))
 
 	log.Printf("Сервер запущен на http://localhost:%d", port)
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
 		log.Fatal("Ошибка запуска сервера:", err)
 	}
